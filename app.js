42daf1290b046e4807c9dccbe6e7ca5d087b029b
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-01-30 23:33:48 +0530

    cors

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const errorMiddleware=require("./middleware/error")
 const bodyParser=require("body-parser")
 const fileUpload=require("express-fileupload")
 const dotenv=require('dotenv')
+
+const cors = require('cors');
 //const path=require('path')
 
 //config
 dotenv.config({path:"./config/config.env"})
 
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
